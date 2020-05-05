179337a24b708b85b33ec39c1f0d18500aeb7734
Kaush Chokshi <karmarider@gmail.com>
2020-05-05 12:40:29 -0400

    working on test latest-stories.component.spec.ts

--- HackerNewsCoreAngular/ClientApp/src/app/latest-stories/latest-stories.component.spec.ts
+++ HackerNewsCoreAngular/ClientApp/src/app/latest-stories/latest-stories.component.spec.ts
@@ -1,3 +1,8 @@
+//
+// Not working
+// try creating a service
+//
+
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LatestStoriesComponent } from './latest-stories.component';
